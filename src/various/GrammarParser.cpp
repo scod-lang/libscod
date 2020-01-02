1c7dc6799dfe450434ed3ce089f30ecce056516f
Philipp Paulweber <p.paulweber@gmail.com>
2020-01-02 16:33:17 +0100

    Happy New Year :tada:

--- src/various/GrammarParser.cpp
+++ src/various/GrammarParser.cpp
@@ -2,7 +2,7 @@
 
 // Skeleton implementation for Bison LALR(1) parsers in C++
 
-// Copyright (C) 2002-2015, 2018-2019 Free Software Foundation, Inc.
+// Copyright (C) 2002-2015, 2018-2020 Free Software Foundation, Inc.
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
